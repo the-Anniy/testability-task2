1d53484d03b3ca64ebb3274af5b38bcc998107b2
Anna Paliy <annpaliy2015@gmail.com>
2024-11-04 14:35:17 +0300

    result method corrected

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,6 @@
 public class BmiService {
 
     public double calculate(double height, double weight) {
-        double bmi;
-        return bmi = weight / (height * height);
+        return (int) (weight / (height * height));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double height = 1.85; // рост в метрах
-        double weight = 64; //вес в килограммах
-        int result = (int) service.calculate(height, weight);
-        System.out.println(result);
+        double height = 1.87; // рост в метрах
+        double weight = 98; //вес в килограммах
+        int bmi = (int) service.calculate(height, weight);
+        System.out.println(bmi);
     }
 }
